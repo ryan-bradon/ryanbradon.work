23f0fa9c3413b623a8e5f1dd3dc8a7af9562dafd
Ryan Bradon <ryan.bradon@gmail.com>
2023-01-21 09:36:44 +0000

    Tested locally, and added wagtail-resume

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -4,12 +4,17 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-91u_*rohfrrvxi^k9%56(6c((lsuf3p^%3ami^@d^oa8oysk+1"
+SECRET_KEY = "*9fs0asd93245opfs90)(45rohfrrvxi^k9%56(6c((lsuf3p^%3ami^@d^oa8oysk+1"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+        "ryanbradon.work",
+        "127.0.0,1",
+        "143.198.246.159"
+        ]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 
 
 try:
--- home/models.py
+++ home/models.py
@@ -1,6 +1,10 @@
 from django.db import models
-
 from wagtail.models import Page
+from wagtail_resume.models import BaseResumePage
+
+
+class ResumePage(BaseResumePage):
+    pass
 
 
 class HomePage(Page):
